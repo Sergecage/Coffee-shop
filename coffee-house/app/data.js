d0fb54b86fcfeb130b0c145d7d5f93ac2c79128a
Serge Cage <sergeycage@rocketmail.com>
2023-12-11 02:54:46 +0300

    feat: finish the menu page 11.12.23

---
+++ coffee-house/app/data.js
@@ -0,0 +1,8 @@
+let coffeeItem = [{
+    id: 'coffee1',
+    img: "../img/pictures/coffee-1.svg",
+    title: "Irish coffee",
+    text: "Fragrant black coffee with Jameson Irish whiskey and whipped milk",
+    subtitle: "$7.00",
+},
+];
